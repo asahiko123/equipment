6c69f3ba2e6e69ea2ec8daf937eab063160528ae
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-04-07 13:57:50 +0900

    facility_user_id tinyint->bigintegerに変更

---
+++ database/migrations/2022_04_07_134132_change_facility_user_id_tinyint_to_unsigned_big_integer_on_equipment_forms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFacilityUserIdTinyintToUnsignedBigIntegerOnEquipmentFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('equipment_forms', function (Blueprint $table) {
+            $table->unsignedBigInteger('facility_user_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('unsigned_big_integer_on_equipment_forms', function (Blueprint $table) {
+            $table->tinyInteger('facility_user_id')->change();
+        });
+    }
+}
