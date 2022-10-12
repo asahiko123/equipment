1fa3968cb915664129c6966916757d4e1c02fe0e
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-10-12 18:55:52 +0900

    auth::route->manual auth routing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,4 @@ class LoginController extends Controller
         return 'name';
     }
 
-    public function showLoginForm(){
-        return view('auth.login');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
-Auth::routes();
+// Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/login','LoginController@showLoginForm')->name('login');
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login');
+Route::post('logout','Auth\LoginController@logout')->name('logout');
+
+Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register','Auth\RegisterController@register');
 
 Route::get('/', function () {
     return view('welcome');
